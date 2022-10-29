e2d9061ea56ecf8b625c33fba0ba9a54f984f29b
YasinCelikSoftware <yasin.celik4@outlook.com>
2022-10-29 15:57:19 +0300

    Is prime exercise made and uploaded.

---
+++ src/thirdweekexercises/IsPrimeExercise.java
@@ -0,0 +1,46 @@
+package thirdweekexercises;
+
+public class IsPrimeExercise {
+    public static void main(String[] args) {
+        test();
+
+    }
+
+    public static boolean isPrime(long number){
+        if(number < 2)
+            return false;
+
+        for(long l = 2; l < sqrt(number); l++){
+
+            if(number%l == 0)
+                return false;
+
+        }
+
+        return true;
+    }
+
+    public static long sqrt(long number){
+        long i;
+        for(i = 1; i<number; i++){
+            if(i*i == number)
+                return i;
+            if(i*i>number)
+                return i-1;
+        }
+
+        return 0;
+
+    }
+
+    public static void test(){
+
+        for(long i = 3167777777l; i < 3167777888l ; i++){
+
+            if(isPrime(i))
+                System.out.println(i);
+
+        }
+
+    }
+}
