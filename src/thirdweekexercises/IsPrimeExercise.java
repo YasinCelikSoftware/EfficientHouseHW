512376c59a82c16c0e5caf39732861ccb1a55aad
YasinCelikSoftware <yasin.celik4@outlook.com>
2022-10-30 06:25:31 +0300

    Is prime exercise loaded.

--- src/thirdweekexercises/IsPrimeExercise.java
+++ src/thirdweekexercises/IsPrimeExercise.java
@@ -3,39 +3,24 @@ package thirdweekexercises;
 public class IsPrimeExercise {
     public static void main(String[] args) {
         test();
-
     }
 
     public static boolean isPrime(long number){
+        if (number == 2)
+            return true;
 
-        if(number < 2)
+        if(number <= 1 || number%2 == 0)
             return false;
 
-        for(long l = 2; l < sqrt(number); l++){
+
+        for(long l = 3; l*l <= number; l+=2){
+
             if(number%l == 0)
                 return false;
 
         }
 
         return true;
-    }
-
-    public static long sqrt(long number){
-
-        long i;
-
-        for(i = 1; i<number; i++){
-
-            if(i*i == number)
-                return i;
-
-            if(i*i>number)
-                return i-1;
-
-        }
-
-        return 0;
-
     }
 
     public static void test(){
