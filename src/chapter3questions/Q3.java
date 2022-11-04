f8aaf65c7005915dbdf72f4f8355221ec411d62e
YasinCelikSoftware <yasin.celik4@outlook.com>
2022-11-04 20:06:37 +0300

    Q3 is upgraded.

--- src/chapter3questions/Q3.java
+++ src/chapter3questions/Q3.java
@@ -9,7 +9,7 @@ public class Q3 {
 
     public static void calculateNewton(){
         java.util.Scanner kb = new Scanner(System.in);
-        final double G = 9.81;
+        double G = 9.81;
 
         while(true) {
             System.out.println("\n\" 0 \" --> Exit");
@@ -19,7 +19,7 @@ public class Q3 {
             System.out.print("Please choose an option : ");
             int choice = kb.nextInt();
 
-            if(choice==0)
+            if(choice == 0)
                 break;
 
 
@@ -29,11 +29,11 @@ public class Q3 {
             System.out.println();
 
             if(choice == 1) {
-                double nOnEarth = carKilogram*G;
+                double nOnEarth = carKilogram * G;
                 System.out.printf("%.2f kg car on earth is %.2f Newton.%n%n",carKilogram,nOnEarth);
             }
             if(choice == 2) {
-                double nOnMoon = (carKilogram*G)/6;
+                double nOnMoon = (carKilogram * G) / 6;
                 System.out.printf("%.2f kg car on moon is %.2f Newton.%n%n",carKilogram,nOnMoon);
             }
 
