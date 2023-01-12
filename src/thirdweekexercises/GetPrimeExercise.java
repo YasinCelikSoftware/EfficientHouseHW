d18b7e9610ed9e5d791230a40df3823f0aed047e
Yasin ÇELİK <107568842+YasinCelikSoftware@users.noreply.github.com>
2023-01-12 05:00:46 +0300

    Update GetPrimeExercise.java

--- src/thirdweekexercises/GetPrimeExercise.java
+++ src/thirdweekexercises/GetPrimeExercise.java
@@ -8,16 +8,13 @@ public class GetPrimeExercise {
     }
 
     public static boolean isPrime(long number){
-        if (number == 2)
-            return true;
-
-        if(number <= 1 || number%2 == 0)
-            return false;
+        if (number % 2 == 0)
+            return number == 2;
 
 
         for(long l = 3; l*l <= number; l+=2){
 
-            if(number%l == 0)
+            if(number % l == 0)
                 return false;
 
         }
